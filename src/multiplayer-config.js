3af24d32f9c0f5361a8dad8a414284081f82b2dd
Tim Zegveld <tim.zegveld@gmail.com>
2026-07-14 16:23:26 +0200

    Connect multiplayer to Render API

--- src/multiplayer-config.js
+++ src/multiplayer-config.js
@@ -1,7 +1,5 @@
 globalThis.ExplodingDinosMultiplayerConfig ??= {
-  // Vervang dit na het deployen van server/server.js, bijvoorbeeld:
-  // apiBase: "https://exploding-dinos-api.onrender.com"
   apiBase: globalThis.location?.hostname === "localhost" || globalThis.location?.hostname === "127.0.0.1"
     ? "http://localhost:3000"
-    : ""
+    : "https://exploding-dinos-api.onrender.com"
 };
